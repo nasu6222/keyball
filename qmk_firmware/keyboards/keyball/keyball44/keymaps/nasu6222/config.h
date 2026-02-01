55cefaedd2d88ef07b6cdf418d286556347ad646
nasu6222 <nasu6222@gmail.com>
2026-02-01 12:30:36 +0900

    Remove tapping and flow tap term configurations
    Removed flow tap term and tapping term configurations.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/nasu6222/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/nasu6222/config.h
@@ -38,8 +38,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
-#define FLOW_TAP_TERM 150
-#define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_TERM 175
 
 
