f95312bdab5219f7ae624825201a6aaee7084d22
nasu6222 <nasu6222@gmail.com>
2026-02-01 12:05:37 +0900

    Modify RGBLIGHT effect and add tapping configurations

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/nasu6222/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/nasu6222/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_SNAKE
 //#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //#    define RGBLIGHT_EFFECT_RGB_TEST
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_TWINKLE
@@ -38,4 +38,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define FLOW_TAP_TERM 150
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM 175
+
 
