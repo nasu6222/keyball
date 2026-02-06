108b764a12dc3dbe5652dbd55f03577b90e0e793
nasu6222 <nasu6222@gmail.com>
2026-02-06 16:47:04 +0900

    Integrate pointing device and update mouse layer handling
    Added pointing device functionality and modified layer behavior.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/nasu6222/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/nasu6222/keymap.c
@@ -17,8 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-
 #include "quantum.h"
+#include "pointing_device.h"
+
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -45,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
+    RGB_TOG  , TG(1)   , _______  , _______  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
                   QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
@@ -69,3 +70,19 @@ void oledkit_render_info_user(void) {
     keyball_oled_render_layerinfo();
 }
 #endif
+
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    // マウスレイヤー（1）のときだけ、トラックボール移動を有効にする
+    if (layer_state_is(1)) {
+        return mouse_report;
+    }
+  
+    mouse_report.x = 0;
+    mouse_report.y = 0;
+    mouse_report.h = 0;
+    mouse_report.v = 0;
+    // クリックも止めたいなら、この行を有効化
+    // mouse_report.buttons = 0;
+
+    return mouse_report;
+}
