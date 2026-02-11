873a34b831d8043f3a9874a1945b39a5f89b26f9
nasu6222 <nasu6222@gmail.com>
2026-02-11 14:51:01 +0900

    Refactor keymap layers for better readability

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/vial/keymap.c
@@ -17,26 +17,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // マウス層（あなたの運用に合わせて残す）
   [1] = LAYOUT_universal(
     SSNP_FRE ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    SSNP_VRT ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
-    SSNP_HOR ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
-                  _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    SSNP_VRT ,  _________ , _________  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
+    SSNP_HOR ,  _________ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _________  , _________  ,
+                  _________  , _________ , _________  ,         _________  , _________  ,                   _________  , _________  , _________       , _________  , _________
   ),
 
   [2] = LAYOUT_universal(
-    _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _______  ,
-    _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
-    _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
-                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
+    _________  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _________  ,
+    _________  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
+    _________  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
+                  KC_0     , KC_DOT  , _________  ,         _________  , _________  ,                   KC_DEL   , _________  , _________       , _________  , _________
   ),
 
   // 旧 Layer3 は RGB/OLED が多いので “スクロール/CPI/保存/ブート” に寄せる
   [3] = LAYOUT_universal(
-    _______  , TG(1)    , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , SCRL_DVI ,                                        _______  , _____
-::contentReference[oaicite:19]{index=19}
-__  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-                  QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
+    _________  , TG(1)    , _________  , _________  , _________  , _________  ,                                        _________  , _________  , _________  , _________  , _________  , _________  ,
+    _________  , _________  , _________  , _________  , _________  , SCRL_DVI ,                                        _________  , _______
+__  , _________  , _________  , _________  , _________  ,
+    _________  , _________  , _________  , _________  , _________  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _________  , KBC_SAVE ,
+                  QK_BOOT  , KBC_RST  , _________  ,        _________  , _________  ,                   _________  , _________  , _________       , KBC_RST  , QK_BOOT
   ),
 };
 
